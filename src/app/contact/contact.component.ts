208d4af8a47e5d1029d71f517dc66974d39481f2
Over The Top <overthetop_for_ever@live.fr>
2022-10-04 16:59:30 +0200

    page mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    email: ['', [Validators.required, Validators.email]],
+    subject: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    message: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(1000)]]
+  });
+
+  nameLength = 0;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
   }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      window.location.href = `mailto:overthetop_for_ever@live.fr?subject=${this.contactForm.value.subject}&body=${this.contactForm.value.message}`;
+      alert('Your message has been sent!');
+      this.contactForm.reset();
+    }
+  }
+
 
 }
